adf60e759e2fbf34aa2f57a8a86dc4a2d160e55f
SatyapriyaMahato <satyapriyamahto@gmail.com>
2023-09-23 21:09:39 +0530

    get data from forkify api

--- src/js/controller.js
+++ src/js/controller.js
@@ -11,3 +11,34 @@ const timeout = function (s) {
 // https://forkify-api.herokuapp.com/v2
 
 ///////////////////////////////////////
+
+const showRecipe = async function () {
+  try {
+    const res = await fetch("https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc886?key=3a79586f-a4e2-46c6-83c3-304592810e6a");
+
+    const data = await res.json();
+
+    if (!res.ok) throw new Error(`${data, message} {${res.status}}`)
+
+    const { recipe } = data.data;
+
+    const recipeData = {
+      cookingTime: recipe.cooking_time,
+      id: recipe.id,
+      imageUrl: recipe.image_url,
+      ingredients: recipe.ingredients,
+      publisher: recipe.publisher,
+      servings: recipe.servings,
+      sourceUrl: recipe.source_url,
+      title: recipe.title
+    }
+
+    console.log(recipeData);
+
+
+  } catch (err) {
+    alert(err);
+  }
+
+}
+showRecipe();
